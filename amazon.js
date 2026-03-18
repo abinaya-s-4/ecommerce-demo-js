01e450730b88c8754e96ba5c418835a5d90094c4
abinaya-s-4 <abinaya.selva2020@gmail.com>
2026-03-18 13:02:17 +0530

    Added jsx

--- amazon.js
+++ amazon.js
@@ -1,26 +1,81 @@
-const products = [{
-  name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  img: 'images/ratings/rating-45.png',
-  ratings: {
-    stars: 4.5,
-    count: 87,
-  },
-  price : 849
-},{
-  name : 'Intermediate Size Basketball',
-  img : 'images/ratings/rating-40.png',
-  ratings :{
-    stars:4 ,
-    count: 127,
-  },
-  price : 499
+  const product = [{
+    name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    img: 'images/ratings/rating-45.png',
+    ratings: {
+      stars: 4.5,
+      count: 87
+    },
+    price : 849
+  },{
+    name : 'Intermediate Size Basketball',
+    img : 'images/ratings/rating-40.png',
+    ratings :{
+      stars:4 ,
+      count: 127
+    },
+    price : 499
 
-},{
-  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  img : 'images/ratings/rating-45.png',
-  ratings :{
-    stars:4.5 ,
-    count: 56,
-  },
-  price : 1799
-}]
+  },{
+    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+    img : 'images/ratings/rating-45.png',
+    ratings :{
+      stars:4.5 ,
+      count: 56
+    },
+    price : 1799
+  }]
+
+  let productsHTML = ''
+
+  forEach((product)=>{
+      productsHTML += `<div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.img}">">
+            </div>
+
+      <div> class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="images/ratings/rating-${product.ratings.stars * 10}.png
+          <div class="product-rating-count link-primary">
+            ${product.ratings.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          ${product.price}
+        </div>
+
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary">
+          Add to Cart
+        </button>
+      </div>`
+
+  })
+
