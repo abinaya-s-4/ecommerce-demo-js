1d34d9250f4f56b7033c37741f510f94adaf4f70
abinaya-s-4 <abinaya.selva2020@gmail.com>
2026-03-28 17:21:20 +0530

    made some changes to test

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart,cart} from '../../data/cart.js'
+import {addToCart,cart,loadFromStorage} from '../../data/cart.js'
 
 describe('test suite: addToCart', () =>{
   it('add an existing product to the cart',() =>{
@@ -7,9 +7,10 @@ describe('test suite: addToCart', () =>{
 
   it('add a new product to the cart',() =>{
     spyOn(localStorage, 'getItem').add.callFake(()=>{
-      return JSON.stringify[];
+      return JSON.stringify([]);
     });
-    console.log(localStorage.getItem('cart'));
+
+    loadFromStorage();
     
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
