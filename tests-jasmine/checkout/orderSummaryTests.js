71adb43dbe4cbefda2b643a33d1b253b88dc1931
abinaya-s-4 <abinaya.selva2020@gmail.com>
2026-03-29 18:09:56 +0530

    ran tests

---
+++ tests-jasmine/checkout/orderSummaryTests.js
@@ -0,0 +1,29 @@
+import { renderOrderSummary } from "../../checkout/orderSummary.js";
+import {loadFromStorage} from '../../data/cart.js'
+
+
+//to create a test suite
+describe('test suite: renderOrderSummary', () =>{
+// to create a test ('name',function )
+  it('displays the cart', ()=>{
+    document.querySelector('.js-test-container').innerHTML =`
+    <div class = "js-cart-summary"></div>
+    `;
+    spyOn(localStorage,'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(()=>{
+    return JSON.stringify([{  
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity : 2,
+    deliveryOptionId : '1' 
+  },{
+    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity : 1,
+    deliveryOptionId : '2'
+  
+    }]);
+  });
+  loadFromStorage();
+  renderOrderSummary();
+  });
+  
+});
