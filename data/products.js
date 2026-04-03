f42158211c53ae83472015942e4beb9e2d462d8f
abinaya-s-4 <abinaya.selva2020@gmail.com>
2026-04-03 08:51:07 +0530

    used inheritance and polymorphism to add Size Chart

--- data/products.js
+++ data/products.js
@@ -26,7 +26,9 @@ class Product {
   getStarsUrl(){
     return `src="images/ratings/rating-${this.rating.stars * 10}.png`
   }
-
+  extraInfoHTML(){
+    return ``;
+  }
 }
 const product1= new Product({
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -42,6 +44,22 @@ const product1= new Product({
       "sports",
       "apparel"
     ]});
+
+
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+    super(productDetails); //inherit parent class
+    this.sizeChartLink = "images/clothing-size-chart.png"
+  }
+  //Method overriding = override or replace the parent method for extraInfoHTML()
+  extraInfoHTML(){
+    return `
+    <a href = "${this.sizeChartLink}" target = "_blank" >Size Chart</a>
+    `;
+  }
+}
+const tshirt = new Clothing();
  
 
 export const product = [
@@ -704,5 +722,8 @@ export const product = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type === "clothing"){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -42,6 +42,8 @@
         </select>
       </div>
 
+      ${product.extraInfoHTML()} 
+      
       <div class="product-spacer"></div>
 
       <div class="added-to-cart">
